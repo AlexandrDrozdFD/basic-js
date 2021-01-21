43902344dbca1be391fc99a6879f03a7d4e96289
Alexandr Drozd <alex.drozd.fd@gmail.com>
2021-01-22 00:18:26 +0300

    feat: implement functoins

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,34 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  const newArr = [];
-  if (Array.isArray(arr)) {
+ 
+  if (!Array.isArray(arr)){
+    throw new Error('Not An Array');
+  } 
+  
+  const arrNew = [];
 
-      for (let i = 0; i <= arr.length - 1; i++) {
-      newArr.push(arr[i]);
-       
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next' && i != arr.length - 1) {
+      i++;
+    } else if ((arr[i] === '--discard-prev') && (arrNew.length !== 0) && (arr[i - 2] !== '--discard-next')) {
+      arrNew.pop();
+    } else if ((arr[i] === '--double-next') && (i < arr.length - 1)) {
+      arrNew.push(arr[i + 1]);
+    } else if ((arr[i] === '--double-prev') && (i !== 0) && (arr[i - 2] !== '--discard-next')) {
+      arrNew.push(arr[i - 1]);
+    } else { 
+      if ((arr[i] === '--discard-next') || (arr[i] === '--discard-prev') || (arr[i] === '--double-next') || (arr[i] === '--double-prev')) {
+        continue;
       }
-      for (let j = 0; j <= newArr.length - 1; j++) {
-          if (newArr[j] === '--double-next' && newArr[j] === newArr[newArr.length - 1] || newArr[j] === '--discard-next' && newArr[j] === newArr[newArr.length - 1]) {
-             newArr.splice(j, 1);
-          } else if (newArr[j] === '--double-next') {
-              newArr[j] = newArr[j + 1];
-          } else if (newArr[j] === '--discard-next') {
-            newArr.splice(j, 2);
-          } 
-          if ((newArr[j] === '--double-prev' && newArr[j] === newArr[0]) || (newArr[j] === '--discard-prev' && newArr[j] === newArr[0])) {
-              newArr.splice(j, 1);
-          } else if (newArr[j] === '--double-prev') {
-              newArr[j] = newArr[j - 1];
-          } else if (newArr[j] === '--discard-prev') {
-            newArr.splice(j-1, 2);
-          } 
-      }
-  } else {
-    throw new Error('There is no array')
+      arrNew.push(arr[i]);
+    }
   }
-  console.log(arr)
-  return newArr;
-
-  // remove line with error and write your code here
+  return arrNew;
 };
